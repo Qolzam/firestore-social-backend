b2337ec9ae8791547a020c6a2a441363ad4d08b6
Qolzam <amir.gholzam@live.com>
2018-07-19 09:53:27 +0700

    [Config] Change firebase index.js path/

--- functions/src/api/emailAPI.ts
+++ functions/src/api/emailAPI.ts
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer')
 
 // Set variable for this project
 // firebase functions:config:set gmail.email="myusername@gmail.com" gmail.password="secretpassword"
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
+const gmailEmail = functions.config().gmail.email
+const gmailPassword = functions.config().gmail.password
 const mailTransport = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -34,10 +34,10 @@ const sendEmail = (email: Email) => {
             .catch((error: any) => {
               console.error('There was an error while sending the email:', error)
               reject(error)
-            });
+            })
       })
 }
 
-export const emailAPI ={
+export const emailAPI = {
     sendEmail
 }
