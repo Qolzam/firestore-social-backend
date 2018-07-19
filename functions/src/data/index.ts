9160f467f0e5208e72ca988596c05675234e56c7
Drew McArthur <drewmcarthur1@gmail.com>
2018-07-18 22:48:13 -0400

    removed argument from intializeApp in index.js generation

--- functions/src/data/index.ts
+++ functions/src/data/index.ts
@@ -1,9 +1,5 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
-export const adminDB = admin.initializeApp(
-    {
-        ...functions.config().firebase,
-         databaseAuthVariableOverride: 'admin'
-    })
+export const adminDB = admin.initializeApp()
 export const firestoreDB = adminDB.firestore()
